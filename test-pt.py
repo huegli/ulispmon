d86dfe0eeaf05d629ba2c3da5a59f60720f8113d
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2019-04-23 08:55:09 -0700

    Prompt-toolkit learning

--- test-pt.py
+++ test-pt.py
@@ -1,6 +1,35 @@
 from __future__ import unicode_literals
-from prompt_toolkit import prompt
+import pygments
+from pygments.lexers.lisp import CommonLispLexer
+from pygments.styles import get_style_by_name
+from pygments.token import Token
+from prompt_toolkit import prompt, PromptSession
+from prompt_toolkit import print_formatted_text, HTML
+from prompt_toolkit.formatted_text import PygmentsTokens
+from prompt_toolkit.lexers import PygmentsLexer
+from prompt_toolkit.styles.pygments import style_from_pygments_cls
 
-if __name__ == '__main__':
-    answer = prompt('Give me some input: ')
-    print('You said: %s' % answer)
+print_formatted_text(HTML('<aaa fg="ansiwhite" bg="ansigreen">White on green</aaa>'))
+
+text = [
+        (Token.Keyword, 'print'),
+        (Token.Punctuation, '('),
+        (Token.Literal.String.Double, '"'),
+        (Token.Literal.String.Double, 'hello'),
+        (Token.Literal.String.Double, '"'),
+        (Token.Punctuation, ')'),
+        (Token.Text, '\n'),
+        ]
+
+print_formatted_text(PygmentsTokens(text))
+
+
+style = style_from_pygments_cls(get_style_by_name('native'))
+session = PromptSession('Enter Lisp: ', lexer=PygmentsLexer(CommonLispLexer), style=style,
+                        include_default_pygments_style=False)
+answer = ''
+
+while (answer != '(bye)'):
+    answer = session.prompt()
+    tokens = list(pygments.lex(answer, lexer=CommonLispLexer()))
+    print_formatted_text(PygmentsTokens(tokens), style=style)
